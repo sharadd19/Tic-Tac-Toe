cb43fa08224d666cf485d68682798d3604e765a5
Sharad <sharadd19@gmail.com>
2024-05-15 18:07:41 +1000

    Added UI to Tic-Tac-Toe game

--- script.js
+++ script.js
@@ -1,99 +1,10 @@
-const GameBoard = (function () {
 
-    // Create a 3x3 board as an array
-    let board = ['','','','','','','','',''];
-    function getBoard() {
-        return board;
-    }
-
-    // We want to add a marker to a specific place on the board with the players marker
-    function addMarker(board, location) {
-        
-        // If there are no empty cells then exit
-        if (boardIsFull(board)){
-            return;
-        };
-        
-        while (board[location] !== '') {
-            
-            location = Number(prompt('There is already a marker there, enter another number between 0 - 8'));             
-        }
-
-        return board.splice(location, 1, Players.playerMarker());
-    }
-
-    function boardIsFull(board){
-
-        return board.filter(cell => cell === '').length === 0;
-    }
-
-    function clearBoard() {
-
-        return board.map(cell => cell === '');
-    }
-
-   return {
-    getBoard: getBoard,
-    addMarker: addMarker, 
-    clearBoard: clearBoard,
-    boardIsFull: boardIsFull
-   }
-   
     
-})();
 
-const Players = (function() {
-
-    const players = [
-        {
-            name: 'Player One',
-            marker: 'X'
-        },
-        {
-            name: 'Player Two',
-            marker: 'O'
-        }
-    ];
-
-    let activePlayer = players[0];
-
-    function getActivePlayer(){
-        return activePlayer;
-    }
-    
-    function playerMarker(){
-
-        return getActivePlayer().marker;
-    }
-
-    function switchPlayers(){
-
-        return activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    }
-
-    return {
-        playerMarker: playerMarker, 
-        getActivePlayer: getActivePlayer, 
-        switchPlayers: switchPlayers
-    }
-
-})();
-
-// Create a method within the protoype of array that gets the indexes of our players marker
-Array.prototype.multiIndexOf = function(marker){
-    let indxs = [];
-    for (let i=0; i <= this.length - 1; i++ ) {
-        if (this[i] === marker) {
-            indxs.push(i);
-        }
-    }
-    return indxs;
-}
-
-// We want to check if the player has their marker in the position required to win
-function win(board) {
-
-    const winningConditions = [
+    let circleTurn;
+    const CIRCLE_CLASS = 'circle';
+    const X_CLASS = 'x';
+    const WINNING_CONDITIONS = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -104,72 +15,104 @@ function win(board) {
         [2, 4, 6]
     ];
 
-    // If the board contains the same symbol at the indexes of any of the conditions above return true
-    const xMarker = board.multiIndexOf('X'); // [0, 1, 2, 3, 7] WIN
-    const oMarker = board.multiIndexOf('O'); // [4, 5, 6, 8] LOSE
-
-    /* row = [0,1,2], for every element in row -> does the xMarker includes those elements 
-         -> so does xMarker includes every element in r
-         does [0,1,2,3,7] includes 0 and 1 and 2 YES */
-    const xWin = winningConditions.some(row => row.every( r => xMarker.includes(r)));
-    const oWin = winningConditions.some(row => row.every( r => oMarker.includes(r)));  
-
-    if (xMarker.length >= 3 && xWin) {
-        
-        return true;   
-    }
-
-    else if (oMarker.length >= 3 && oWin) {
-        
-        return true;
-    }
+    const cells = document.querySelectorAll('[data-cell]');
+    const board = document.getElementById('board');
+    const winningMessageDiv = document.querySelector('[data-winning-message-text]');
+    const winningMessage = document.getElementById('winning-message');
+    const restartButton = document.getElementById('restart-button');
     
-    return false;
-}
+    startGame();
 
-function playRound() {
+    
+    function startGame(){
 
-    let board = GameBoard.getBoard(); // ['','','','','','','','','']
+        circleTurn = false;
+        cells.forEach(cell => {
 
-    // GETTING CAUGHT IN HERE
-    while (!GameBoard.boardIsFull(board)) {
-        Players.playerMarker(); // 'X'
-        let location = Number(prompt('Pick a number between 0 - 8')); // 4, 0
-        GameBoard.addMarker(board, location);
-        
-        //Check for wins
-        if (win(board)){
-            const winMessage = `${Players.getActivePlayer().name} wins!!`;
-            alert(winMessage);
-            endGame();
-        }
-        else if (GameBoard.boardIsFull(board)){
-            const tieMessage = 'Game is a tie!'
-            alert(tieMessage);
-            endgame();
-        }
-        Players.switchPlayers();
+            // Set the board up 
+            cell.classList.remove(X_CLASS);
+            cell.classList.remove(CIRCLE_CLASS);
+            cell.removeEventListener('click', handleClick);
+
+            cell.addEventListener('click', handleClick, {once: true})
+        });
+        setBoardHoverClass();
+
+        //Set the board up 
+        winningMessage.classList.remove('show');
+
     }
 
-   // endgame(currentPlayer, board);
-    
-}
+    // This function is to 
+    function handleClick(e) {
+        const currentCell = e.target;
+        const currentMarker = circleTurn ? CIRCLE_CLASS: X_CLASS
 
-function startGame() {
-    // clear the board
-    GameBoard.clearBoard();
+        // Place marker
+        placeMarker(currentCell, currentMarker);
+        // Check for wins or draws
+        if (checkWin(currentMarker)){
+            endGame(false);
+        } 
+        else if (isDraw()){
+            endGame(true);
+        }
+        else{
+            // Switch turns 
+            switchTurn();
+            setBoardHoverClass();
+        }
 
-    // play the round
-    playRound();
-}
+    }
 
-function endgame(){
-    // if (board.filter(cell => cell === '').length !== 0)
-    // {
-    //     const winMessage = `${Players.getActivePlayer().name} wins!!`;
-    //     console.log(winMessage);
-    // }
-    GameBoard.clearBoard();
-}
+    restartButton.addEventListener('click', startGame);
 
-startGame();
+    // If the board is full we draw
+    function isDraw(){
+        return [...cells].every(cell => {
+            return cell.classList.contains(X_CLASS) || 
+            cell.classList.contains(CIRCLE_CLASS)
+        });
+    }
+
+    function endGame(draw){
+        // Clear the board if the game is draw 
+        if (draw){
+            winningMessageDiv.innerText = "Draw!"
+        } else{
+            // Show winning message 
+            winningMessageDiv.innerText = `${circleTurn ? "O's" : "X's"} Win!`
+        }
+        
+        winningMessage.classList.add('show')
+
+        
+    }
+
+    function placeMarker(currentCell, currentMarker){
+        currentCell.classList.add(currentMarker);
+    }
+
+    function switchTurn(){
+        circleTurn = !circleTurn;
+    }
+
+    function setBoardHoverClass(){
+        board.classList.remove(CIRCLE_CLASS);
+        board.classList.remove(X_CLASS);
+        if (circleTurn) {
+            board.classList.add(CIRCLE_CLASS);
+        }
+        else {
+            board.classList.add(X_CLASS);
+        }
+    }
+
+    // If any of the cells appear in the winning combinations, we have won.
+    function checkWin(currentClass){
+        return WINNING_CONDITIONS.some(combination => {
+            return combination.every(index => {
+                return cells[index].classList.contains(currentClass);
+            })
+        })
+    }
